23046b2b9b3bdae452cab8af8b0ecdd2dd53e433
Truite Séchée <truite.berthail@gmail.com>
2024-08-12 15:59:51 +0200

    feat: add /hall_of_shame command to display top bulliers

---
+++ src/commands/hall_of_shame.ts
@@ -0,0 +1,40 @@
+
+import { ApplicationCommandOptionType, ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
+import { getHallOfShame } from "../utils/database";
+
+export default {
+    name: "hall",
+    description: "List members according to a parameter",
+    options: [
+        {
+            name: "of",
+            description: "Parameter",
+            type: ApplicationCommandOptionType.SubcommandGroup,
+            options: [
+                {
+                    name: "shame",
+                    description: "List the best bulliers",
+                    type: ApplicationCommandOptionType.Subcommand,
+                    execute: async (interaction: ChatInputCommandInteraction) => {
+                        
+                        const hallOfShame = [];
+                        const rows = getHallOfShame().toSorted((itemA, itemB) => (itemB?.bully_count ?? 0) - (itemA?.bully_count ?? 0));
+                        for (let i = 0; i < rows.length && i < 10; i++) {
+                            hallOfShame.push(`\`${i+1}.\` \`${rows[i]?.bully_count}\` ${(rows[i]?.user_id ? `<@${rows[i]?.user_id}>` : "[inconnu]")} <:thumb_up_happy_face:1269679946796765310> `)
+                        }
+
+                        const embed = new EmbedBuilder()
+                            .setColor(0x5a8c3f)
+                            .setTitle("Hall of Shame - Top bulliers")
+                            .setDescription(hallOfShame.join("\n")); // TODO: translation
+                        
+                        await interaction.reply({
+                            embeds: [embed],
+                            ephemeral: true
+                        })
+                    }
+                }
+            ]
+        }
+    ]
+};
